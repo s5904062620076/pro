3a2ae4300c255e88b86a0d79a59463bb45e7a3fd
viewky <view_sj@hotmail.com>
2019-11-19 14:58:59 +0700

    hey

---
+++ Login.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <!-- UIkit CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.min.css" />
+        <link rel="stylesheet" href="css/Box.css" />
+        <!-- UIkit JS -->
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js"></script>
+       
+    </head>
+<body>
+    <div class="h-home">
+        <?php include 'views/Header.html';?>
+    </div>
+
+    <div class="">
+        <div class=" uk-position-center uk-card uk-card-default uk-card-body ">
+            <form>
+
+            <div class=" uk-margin ">
+                <div class="uk-width-large uk-inline">
+                    <span class="uk-form-icon" uk-icon="icon: user"></span>
+                    <input class="uk-input" type="text">
+                </div>
+            </div>
+            <div class=" uk-margin">
+                <div class="uk-width-large uk-inline">
+                    <span class="uk-form-icon uk-form-icon-flip" uk-icon="icon: lock"></span>
+                    <input class="uk-input " type="text">
+                </div>
+            </div>
+
+            </form>
+        </div>
+    </div>
+    
+</body>
+<html>
