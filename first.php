9e9a13ddaaf7e405475e33a9987aa9a733b6c6e5
viewky <view_sj@hotmail.com>
2019-11-19 14:49:20 +0700

    yeh

---
+++ first.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <link rel="stylesheet" href="css/Box.css" />
+</head>
+<body>
+    <div>
+    <div class="sidebar">
+            <h1>Some text.</h1>
+        </div>
+        <div class="Nav">
+            <h1>Nav</h1>
+        </div>
+        <div class="h">
+        <?php include 'views/Header.html';?>
+        </div>
+        <div class="contend">
+            <h1>Contend</h1>
+        </div>
+    </div>
+</body>
+</html>
